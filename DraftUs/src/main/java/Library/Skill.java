dcb18b3298e859399fca0525d9c250028db593f6
techieanki1247 <ankit.aloni9@gmail.com>
2023-01-12 20:49:52 +0530

    Updated code Library

--- DraftUs/src/main/java/Library/Skill.java
+++ DraftUs/src/main/java/Library/Skill.java
@@ -1,4 +1,40 @@
 package Library;
 
-public class Skill {
+import Utils.AppUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Skill extends AppUtils
+{
+
+    public boolean addSkill(String skill)
+    {
+
+        driver.findElement(By.linkText("Skills")).click();
+        driver.findElement(By.linkText("Add New Skill")).click();
+        driver.findElement(By.name("name")).sendKeys(skill);
+
+
+        WebElement sk = driver.findElement(By.linkText("Skills"));
+        List<WebElement> rows,cols;
+        rows = sk.findElements(By.tagName("tr"));
+
+        boolean isskillpresent = false;
+        for(int i=1;i<rows.size();i++)
+        {
+            cols = rows.get(i).findElements(By.tagName("td"));
+            if(cols.get(1).getText().contains(skill))
+            {
+                isskillpresent= true;
+                break;
+            }
+        }
+        return isskillpresent;
+
+
+
+    }
+
 }
